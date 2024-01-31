13436000526e3322f3f2c70638d31e35c65c7f8a
Maruan Yarbouh <146507031+maruangc@users.noreply.github.com>
2024-01-31 03:10:51 +0000

    Finish

--- src/models.py
+++ src/models.py
@@ -1,18 +1,59 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import datetime
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class Type(enum.Enum):
+    image = 'image'
+    video = 'video'
+    link = 'link'
+    text = 'text'
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(100), nullable=False)
+    name = Column(String(100), nullable=False)
+    lastname = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    password = Column(String(300), nullable=False)
+    address_id = Column(Integer, ForeignKey('address.id'))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    created = Column(DateTime, default=datetime.datetime.now)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum(Type), nullable=False)
+    text_or_url = Column(String(300), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(300), nullable=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    Follower_id = Column(Integer, ForeignKey('person.id'))
+    follower_date = Column(DateTime, nullable=True)
+    follow_to_id = Column(Integer, ForeignKey('person.id'))
+    follow_to_date = Column(DateTime, nullable=True)
 
 class Address(Base):
     __tablename__ = 'address'
